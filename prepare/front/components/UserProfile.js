1966e4d77a86f92d0d6163b9c0b6623300d778f5
miya <miyachan84@gmail.com>
2022-06-03 08:33:41 +0900

    add

--- prepare/front/components/UserProfile.js
+++ prepare/front/components/UserProfile.js
@@ -1,9 +1,27 @@
-import React from "react";
-import { Card } from 'antd';
+import React, { useCallback } from "react";
+import { Avatar, Card, Button } from 'antd';
 
 
-const UserProfile = () => {
-
+const UserProfile = ({ setIsLoggedIn }) => {
+    const onLogOut = useCallback(() => {
+        setIsLoggedIn(false);
+    }, []);
+      
+    return (
+        <Card
+        actions={[
+            <div key="twit">짹짹<br/>0</div>,
+            <div key="fllowing">팔로잉<br/>0</div>,
+            <div key="fllower">팔로워<br/>0</div>,
+        ]}
+        >
+            <Card.Meta
+            avatar={<Avatar>Miya</Avatar>}
+            title="Miya"
+            />
+            <Button onClick={onLogOut}>Logout</Button>
+        </Card>
+    );
 }
 
 export default UserProfile;
